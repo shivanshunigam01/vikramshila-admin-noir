ca0defbb46050a25854d3b3e13accde6bf1dfe0d
shivanshu nigam <shivanshuN@200oksolutions.com>
2025-09-20 09:42:35 +0530

    .oaslasndudhs;oudsh;oduh;ou

--- src/services/dseService.tsx
+++ src/services/dseService.tsx
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getDSEList = async () => {
-  const res = await axios.get(`${API_BASE}/auth/get-dse`);
+  const res = await axios.get(`${API_URL}/auth/get-dse`);
   return res.data.items; // only items array
 };
 
 export const getDSELocation = async (dseId) => {
-  const res = await axios.get(`${API_BASE}/tracking/latest/${dseId}`);
+  const res = await axios.get(`${API_URL}/tracking/latest/${dseId}`);
   return res.data;
 };
