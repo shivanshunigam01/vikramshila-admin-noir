404219f221ad117d458dcf4c7d4ff51f11bbbb49
shivanshu nigam <shivanshuN@200oksolutions.com>
2025-10-19 00:46:18 +0530

    asd

--- src/services/schemeService.tsx
+++ src/services/schemeService.tsx
@@ -3,6 +3,7 @@ import axiosInstance from "@/api/axiosInstance";
 const API_URL = import.meta.env.VITE_API_URL;
 
 
+
 export const getSchemes = async () => {
   try {
     const res = await axiosInstance.get(`${API_URL}/schemes`);
